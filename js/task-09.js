aeb92010c396da4dba88b5e9fd73d8fde7d48dd9
Iryna Zaliska <zaliska@gmail.com>
2023-02-27 11:59:43 +0200

    hw-06-2

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtnEl = document.querySelector('.change-color');
+const bgColorEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+changeColorBtnEl.addEventListener('click', (event) => {
+  const currentBgColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = currentBgColor;
+  bgColorEl.textContent = currentBgColor;
+});
